19f4cb9d836fbcb18a0976a21d0ac7f9e5fe4fd0
Anastasiya Yuragina <dark25555@gmail.com>
2016-10-10 12:20:29 +0300

    added rx to CountryModel

--- app/src/main/java/com/anastasiyayuragina/testproject/screen/country_list/CountriesModel.java
+++ app/src/main/java/com/anastasiyayuragina/testproject/screen/country_list/CountriesModel.java
@@ -10,13 +10,13 @@ import com.anastasiyayuragina.testproject.ourDataBase.CountryItem;
 import com.raizlabs.android.dbflow.sql.language.Select;
 import java.util.List;
 import java.util.Map;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.jackson.JacksonConverterFactory;
 import rx.Observable;
-import rx.Subscription;
+import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by anastasiya yuragina on 8/2/16.
@@ -32,32 +32,33 @@ class CountriesModel implements CountriesMvp.Model {
 
         if (countryTable.isEmpty()) {
             Retrofit retrofit = new Retrofit.Builder()
+                    .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                     .baseUrl("http://api.worldbank.org/")
                     .addConverterFactory(JacksonConverterFactory.create())
                     .build();
             CountriesAPIService service = retrofit.create(CountriesAPIService.class);
-//            Observable<CountryItem> itemCall = service.loadItem(pageParam(String.valueOf(page)));
-            Observable.just(
-                    service.loadItem(pageParam(String.valueOf(page)))
-                    .subscribe(this::onLoadCountryItem)
-            );
 
-            listener.onDataLoaded(list, pageInfo);
-            saveIntoDB(list, page);
-//            itemCall.enqueue(new Callback<CountryItem>() {
-//                @Override
-//                public void onResponse(Call<CountryItem> call, Response<CountryItem> response) {
-//                    CountryItem = response.body();
-//
-//                    listener.onDataLoaded(CountryItem.getCountryList(), CountryItem.getPageInfo());
-//
-//                    saveIntoDB(CountryItem.getCountryList(), page);
-//                }
-//
-//                @Override
-//                public void onFailure(Call<CountryItem> call, Throwable t) {
-//                }
-//            });
+            Observable<CountryItem> itemCall = service.loadItem(pageParam(String.valueOf(page)));
+
+            itemCall.subscribeOn(Schedulers.newThread())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Subscriber<CountryItem>() {
+                        @Override
+                        public void onCompleted() {
+                        }
+
+                        @Override
+                        public void onError(Throwable e) {
+                        }
+
+                        @Override
+                        public void onNext(CountryItem countryItem) {
+                            list = countryItem.getCountryList();
+                            pageInfo = countryItem.getPageInfo();
+                            listener.onDataLoaded(list, pageInfo);
+                            saveIntoDB(list, page);
+                        }
+                    });
         } else {
             PageInfo pageInfo = new PageInfo();
             pageInfo.setPage(page);
@@ -74,11 +75,6 @@ class CountriesModel implements CountriesMvp.Model {
         urlParams.put("page", page);
 
         return urlParams;
-    }
-
-    private void onLoadCountryItem(CountryItem countryItem) {
-        list = countryItem.getCountryList();
-        pageInfo = countryItem.getPageInfo();
     }
 
     private void saveIntoDB (List<Country> itemCountry, int page) {
